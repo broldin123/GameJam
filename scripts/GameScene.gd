45f844d9a9467d4a9bc74ab5a5510b3a80fd63b4
mrVentures <matthewxventures@gmail.com>
2024-04-13 12:21:17 -0700

    How to play screen

--- scripts/GameScene.gd
+++ scripts/GameScene.gd
@@ -2,17 +2,21 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	DialogueManager.show_example_dialogue_balloon(load("res://dialogues/introWifeChat.dialogue"))
+	print("Starting game scene")
+	MyGlobals.wifeChatFinished = false
 	$Scale/Control/WifeChat/btnBeginDay.visible = false
+	$Scale/Control/WifeChat.visible = true
+	DialogueManager.show_example_dialogue_balloon(load("res://dialogues/introWifeChat.dialogue"))
+	$Scale/ScalePlateRight/House/Skeledog/animSkeleDog.play("skeleDog walk cycle")
 
 func _on_btn_main_menu_pressed():
 	get_tree().change_scene_to_file("res://scenes/TitleScene.tscn")
 
 func _on_btn_start_home_pressed():
-	$Scale/ScaleAnim.play("RightDownToUp")
+	$Scale/animScaleUpDown.play("RightDownToUp")
 
 func _on_btn_start_work_pressed():
-	$Scale/ScaleAnim.play("RightUpToDown")
+	$Scale/animScaleUpDown.play("RightUpToDown")
 	
 func _on_btn_end_screen_pressed():
 	get_tree().change_scene_to_file("res://scenes/EndScene.tscn")
@@ -24,7 +28,7 @@ func _input(_event):
 #hides the wife chat when we push the begin day button
 func _on_btn_begin_day_pressed():
 	$Scale/Control/WifeChat.visible = false
-	$Scale/ScaleAnim.play("RightDownToUp")
+	$Scale/animScaleUpDown.play("RightDownToUp")
 	
 #shows the button after we finish the wife chat
 #called every frame
