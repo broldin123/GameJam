9af6997178c01aefde5b141fd8761636f87b959b
CYBORGSMOKEY\scott <scottxmulligan@gmail.com>
2024-04-13 16:13:49 -0400

    Fixed merge issues

--- scripts/global.gd
+++ scripts/global.gd
@@ -5,10 +5,8 @@ var player_dodge = false
 var questStatus_DontBurnTheOmlette = "None"
 var questStatus_DefendTheCrypt = "None"
 var wifeChatFinished = false
-<<<<<<< HEAD
-var last_world_position = Vector2(0,0)
-=======
+
 
 var questResults = [""]
 var poopNum = 1
->>>>>>> c130c300e489763256bf4cad632ed7bc738b5724
+
