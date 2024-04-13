5f36e6c42da4caa3360c3fdbcbbab090a0e68ed6
CYBORGSMOKEY\scott <scottxmulligan@gmail.com>
2024-04-13 16:06:25 -0400

    Portal - Combat System

--- enemy.gd
+++ enemy.gd
@@ -4,27 +4,38 @@ var speed = 50
 var player_chase = false
 var player = null
 
-
-var health = 3
+var totalHealth = 10
+var health = 10
 var player_inattack_zone = false
 var can_take_damage = true
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	deal_with_damage()
 	
 	if player_chase:
 		position += (player.position - position)/speed
 		
-		$AnimatedSprite2D.play("walk")
+		if player_inattack_zone == true:
+			$AnimatedSprite2D.play("side_attack")
+		else:
+			$AnimatedSprite2D.play("walk")
+		
+		if(player.position.x - position.x <0):
+			$AnimatedSprite2D.flip_h = true
+		else:
+			$AnimatedSprite2D.flip_h = false
 	else:
 		$AnimatedSprite2D.play("idle")
+		
+
+
 
 func _on_detection_area_body_entered(body):
 	player = body
 	player_chase = true
 
 
-func _on_detection_area_body_exited(body):
+func _on_detection_area_body_exited(_body):
 	player = null
 	player_chase = false
 
@@ -43,12 +54,14 @@ func _on_enemy_hitbox_body_exited(body):
 
 func deal_with_damage():
 	if player_inattack_zone and global.player_current_attack == true:
+		
 		if can_take_damage == true:
 			health = health - 1
 			$take_damage_cooldown.start()
 			can_take_damage = false
-			print("hero health =", health)
+			print("hero health = ", health, " out of ", totalHealth)
 			if health <= 0:
+				$AnimatedSprite2D.play("death")
 				self.queue_free()
 
 
--- scripts/global.gd
+++ scripts/global.gd
@@ -1,7 +1,8 @@
 extends Node
 
 var player_current_attack = false
+var player_dodge = false
 var questStatus_DontBurnTheOmlette = "None"
 var questStatus_DefendTheCrypt = "None"
 var wifeChatFinished = false
-
+var last_world_position = Vector2(0,0)
