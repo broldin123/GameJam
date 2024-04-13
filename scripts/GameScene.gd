db543ef3bfe9fcafa2bcb0f68e3b2d1003ea2078
mrVentures <matthewxventures@gmail.com>
2024-04-12 23:44:17 -0700

    Font, title screen graphics, music

---
+++ scripts/GameScene.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_btn_main_menu_pressed():
+	get_tree().change_scene_to_file("res://scenes/TitleScene.tscn")
+
+func _on_btn_start_home_pressed():
+	$Scale/ScaleAnim.play("RightDownToUp")
+
+func _on_btn_start_work_pressed():
+	$Scale/ScaleAnim.play("RightUpToDown")
+	
+func _on_btn_end_screen_pressed():
+	get_tree().change_scene_to_file("res://scenes/EndScene.tscn")
